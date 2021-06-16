cbbd96809bb7fd2f389f43fe4dab3fee31da3380
Gustavo Adolfo Morales <tavomorales88@gmail.com>
2021-06-16 10:34:48 -0500

    detalles

--- NewApi/Models/Response/DetailTransactionRes.cs
+++ NewApi/Models/Response/DetailTransactionRes.cs
@@ -83,5 +83,76 @@ namespace SER.EpaycoSdk.NewApi.Models.Response
 
         [JsonPropertyName("email")]
         public string Email { get; set; }
+        [JsonPropertyName("log")]
+        public Log Log { get; set; }
+    }
+
+    public partial class Log
+    {
+        [JsonPropertyName("x_cust_id_cliente")]
+        public string XCustIdCliente { get; set; }
+
+        [JsonPropertyName("x_ref_payco")]
+        public string XRefPayco { get; set; }
+
+        [JsonPropertyName("x_id_factura")]
+        public string XIdFactura { get; set; }
+
+        [JsonPropertyName("x_id_invoice")]
+        public string XIdInvoice { get; set; }
+
+        [JsonPropertyName("x_description")]
+        public string XDescription { get; set; }
+
+        [JsonPropertyName("x_amount")]
+        public string XAmount { get; set; }
+
+        [JsonPropertyName("x_amount_country")]
+        public string XAmountCountry { get; set; }
+
+        [JsonPropertyName("x_amount_ok")]
+        public string XAmountOk { get; set; }
+
+        [JsonPropertyName("x_tax")]
+        public string XTax { get; set; }
+
+        [JsonPropertyName("x_amount_base")]
+        public string XAmountBase { get; set; }
+
+        [JsonPropertyName("x_currency_code")]
+        public string XCurrencyCode { get; set; }
+
+        [JsonPropertyName("x_bank_name")]
+        public string XBankName { get; set; }
+
+        [JsonPropertyName("x_cardnumber")]
+        public string XCardnumber { get; set; }
+
+        [JsonPropertyName("x_quotas")]
+        public string XQuotas { get; set; }
+
+        [JsonPropertyName("x_respuesta")]
+        public string XRespuesta { get; set; }
+
+        [JsonPropertyName("x_response")]
+        public string XResponse { get; set; }
+
+        [JsonPropertyName("x_approval_code")]
+        public string XApprovalCode { get; set; }
+
+        [JsonPropertyName("x_transaction_id")]
+        public string XTransactionId { get; set; }
+
+        [JsonPropertyName("x_fecha_transaccion")]
+        public string XFechaTransaccion { get; set; }
+
+        [JsonPropertyName("x_transaction_date")]
+        public string XTransactionDate { get; set; }
+
+        [JsonPropertyName("x_cod_respuesta")]
+        public int CodRespuesta { get; set; }
+
+        [JsonPropertyName("x_cod_response")]
+        public int CodResponse { get; set; }
     }
 }
